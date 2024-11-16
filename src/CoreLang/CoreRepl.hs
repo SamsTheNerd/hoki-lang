69eefada8130ed84091210e14e37a27f730ba69a
SamsTheNerd <sam@samsthenerd.com>
2024-11-16 04:57:59 -0500

    she lives in the crepl now (got the crepl set up with haskeline for nice inputs)

--- src/CoreLang/CoreRepl.hs
+++ src/CoreLang/CoreRepl.hs
@@ -1,50 +1,50 @@
 module CoreLang.CoreRepl where
-import Control.Monad.State (StateT (..), lift, put, get, MonadIO (liftIO), gets, withStateT)
+import Control.Monad.State.Strict (StateT (..), lift, put, get, MonadIO (liftIO), gets, withStateT)
 import CoreLang.CoreSorts
 import CoreLang.CoreParser (readProgramFile, parseExpr)
 import CoreLang.PrimLoader (evalProgram)
 import Data.Bifunctor (Bifunctor(..))
 import Control.Monad (void)
+import System.Console.Haskeline
+import Data.Maybe (fromMaybe)
 
 -- Core Repl Monad
-type CReplad a = StateT (FilePath, Program) IO a
+type CReplad a = InputT (StateT (FilePath, Program) IO) a
 
 creplDispatch :: Char -> String -> CReplad ()
 creplDispatch 'l' arg = do
-    errOrProg <- lift $ readProgramFile arg
+    errOrProg <- liftIO $ readProgramFile arg
     case errOrProg of
-        (Left err) -> lift $ putStrLn ("error: " ++ show err)
-        (Right prog) -> put (arg, prog) >> lift (putStrLn $ "loaded file: " ++ arg)
+        (Left err) -> liftIO $ putStrLn ("error: " ++ show err)
+        (Right prog) -> lift $ put (arg, prog) >> liftIO (putStrLn $ "loaded file: " ++ arg)
 creplDispatch 'r' _ = do
-    (fn, oldP) <- get
-    errOrProg <- lift $ readProgramFile fn
+    (fn, oldP) <- lift get
+    errOrProg <- liftIO $ readProgramFile fn
     case errOrProg of
-        (Left err) -> lift $ putStrLn ("error: " ++ show err)
-        (Right prog) -> put (fn, prog) >> lift (putStrLn $ "reloaded file: " ++ fn)
+        (Left err) -> outputStrLn ("error: " ++ show err)
+        (Right prog) -> lift $ put (fn, prog) >> liftIO (putStrLn $ "reloaded file: " ++ fn)
 
-creplEval :: String -> Program -> IO ()
+creplEval :: String -> Program -> CReplad ()
 creplEval inp prog = case parseExpr inp of
-    (Left err) -> putStrLn ("parse error: " ++ show err)
+    (Left err) -> outputStrLn ("parse error: " ++ show err)
     (Right expr) -> do
-        res <- evalProgram prog expr
+        res <- liftIO $ evalProgram prog expr
         case res of
-            (Left err) -> putStrLn ("error: " ++ err)
-            (Right resVal) -> print resVal
-    -- do
-    -- expr <- ()
-    -- errOrRes <- evalProgram prog expr
-
+            (Left err) -> outputStrLn ("error: " ++ err)
+            (Right resVal) -> outputStrLn $ show resVal
 
 creplLoop :: CReplad ()
 creplLoop = do
-    lift $ putStr "<3: "
-    inp <- lift getLine
+    -- liftIO $ putStr "<3: "
+    inp <- fromMaybe "" <$> getInputLine "<3: "
     case inp of
-        (':':c:' ':arg) -> creplDispatch c arg
-        _ -> gets snd >>= lift . creplEval inp
+        (':':c:' ':arg) -> creplDispatch c arg -- TODO: this is disgusting
+        (':':c:arg) -> creplDispatch c arg
+        "" -> return ()
+        _ ->  lift (gets snd) >>= creplEval inp
     creplLoop
 
-startCrepl = do
-    void $ runStateT creplLoop ("", [])
+startCrepl :: IO ()
+startCrepl = void $ runStateT (runInputT defaultSettings creplLoop) ("", [])
 
-main = startCrepl
+-- main = startCrepl
