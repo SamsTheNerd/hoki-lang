f81fbb9253f2a74052e245e8a808dd57aafbce06
mira <fish-in-the-sea@users.noreply.github.com>
2024-12-11 19:21:09 -0500

    Updating Show

--- src/Hoki/FrontSorts.hs
+++ src/Hoki/FrontSorts.hs
@@ -1,3 +1,4 @@
+
 module Hoki.FrontSorts where
 
 import Hoki.FrontUtil
@@ -15,18 +16,37 @@ data Type = Tsig Ident | Tabb [Type] Type
 
 data Args = Avar Ident
           | Alit Literal
-          deriving(Show, Eq)
+          deriving(Eq)
+
 
 data Literal = LDec  Double
              | LInt  Integer
              | LChar Char
              | LStr  String
              | LBool Bool
-             deriving (Show, Eq)
+             deriving (Eq)
 
 newtype Program = Prog [Expr] deriving (Show)
+
+instance Show Literal where
+    show (LDec d)  = show d 
+    show (LInt i)  = show i
+    show (LChar c) = show c
+    show (LStr s)  = show s
+    show (LBool b) = show b
+
+instance Show Args where
+    show (Avar a) = a
+    show (Alit l) = show l
+
+
 
 instance Show Expr where
     show (Evar v) = "Evar" ++ v
     show (Elit l) = show l
     show (Eabb n a b t) = "(" ++ joinWith' " " a ++ n ++ " <- " ++  joinWith "; " (map show b) ++ ")"
+    show (Eapp i a) = "(" ++ joinWith " " (map show  a) ++ ") " ++ i
+
+
+
+
